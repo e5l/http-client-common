47490f9bc7d8ecab6d9e7f585aa7908aca8cdd96
e5l <leonid.stashevsky@gmail.com>
2018-03-16 09:46:36 +0300

    Update project configuration:
    * remove CMake project
    * update coroutines version
    * cleanup

--- cmake-build-debug/CMakeFiles/3.10.2/CMakeKotlinCompiler.cmake
+++
@@ -1,5 +0,0 @@
-set(CMAKE_Kotlin_COMPILER "/Users/leonid/.konan/kotlin-native-macos-0.6.1/bin/konanc")
-set(CMAKE_Kotlin_COMPILER_LOADED 1)
-
-set(CMAKE_Kotlin_SOURCE_FILE_EXTENSIONS kt;def)
-set(CMAKE_Kotlin_COMPILER_ENV_VAR "KOTLIN_COMPILER")
--- cmake-build-debug/CMakeFiles/http-client-common.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/http-client-common"
-  "CMakeFiles/libs/http-client-common.klib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang Kotlin)
-  include(CMakeFiles/http-client-common.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
